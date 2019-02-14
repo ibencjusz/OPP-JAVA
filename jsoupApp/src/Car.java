dbf67ee666a31152a9f6990519ea9b14438d9da8
ibencjusz <34000046+ibencjusz@users.noreply.github.com>
2019-02-14 14:27:29 +0100

    add class
    car

---
+++ jsoupApp/src/Car.java
@@ -0,0 +1,66 @@
+public class Car {
+
+    private String name;
+    private String shortDescription;
+    private double price;
+    private String year;
+    private String distance;
+    private String capacity;
+    private String engine;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+}
--- jsoupApp/src/Main.java
+++ jsoupApp/src/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
         System.out.println(d.title());
 
-        
+
         //a with href
 
         /*
